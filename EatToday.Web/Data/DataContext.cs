f3163f76837ffdd301a582dddbadfdc0167dd42c
ALEX JHOJAN BOLIVAR VARGAS <alexbolivar240835@correo.itm.edu.co>
2019-08-24 16:15:30 -0500

    DataContext

---
+++ EatToday.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using EatToday.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatToday.Web.Data
+{
+    public class DataContext : DbContext
+    {
+            public DataContext(DbContextOptions<DataContext> options) : base(options)
+            {
+            }
+            public DbSet<Customer> Customers { get; set; }
+  
+    }
+}
