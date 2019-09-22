d73702f601232f360f35b3e958edd7ddfd8e263e
ALEX JHOJAN BOLIVAR VARGAS <alexbolivar240835@correo.itm.edu.co>
2019-09-21 21:05:28 -0500

    Part 52 - Recover Password

--- EatToday.Web/Helpers/IUserHelper.cs
+++ EatToday.Web/Helpers/IUserHelper.cs
@@ -35,5 +35,10 @@ namespace EatToday.Web.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+
     }
 }
