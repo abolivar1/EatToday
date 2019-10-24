b20a963dd33e921d2a0ddf8d23275065f03ffecc
Juan Martínez <juan.a.martinez@accenture.com>
2019-10-24 16:01:20 -0500

    Commit after they fuck

--- EatToday.Prism/EatToday.Prism/ViewModels/ChooseIngredientsPageViewModel.cs
+++ EatToday.Prism/EatToday.Prism/ViewModels/ChooseIngredientsPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Prism.Commands;
+﻿using EatToday.Common.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace EatToday.Prism.ViewModels
@@ -10,11 +12,56 @@ namespace EatToday.Prism.ViewModels
     public class ChooseIngredientsPageViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
-
+        private ObservableCollection<IngredientResponse> _ingredients;
+        private string _itemSelected;
+        private List<string> _itemsSelected;
         public ChooseIngredientsPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             _navigationService = navigationService;
             Title = "Choose Ingredients";
         }
+
+        public string ItemsSelected
+        {
+
+            get => _itemSelected;
+            set
+            {
+                _itemSelected = value;
+                this.AddItems(value);
+            }
+        }
+
+        
+
+        public ObservableCollection<IngredientResponse> IngredientsCollection
+        {
+            get => _ingredients;
+            set => SetProperty(ref _ingredients, value);
+        }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+
+            if (parameters.ContainsKey("ingredient"))
+            {
+                var ingredients = parameters.GetValue<List<IngredientResponse>>("ingredient");
+
+                IngredientsCollection = new ObservableCollection<IngredientResponse>(ingredients.Select(r => new IngredientResponse()
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+
+                }).ToList());
+            }            
+        }
+
+        private void AddItems(string value)
+        {
+            _itemsSelected.Add(value);
+            Console.WriteLine(value);
+        }
     }
 }
+
