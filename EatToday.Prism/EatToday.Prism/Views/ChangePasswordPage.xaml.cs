7c14f8e19eb3e06e00d7cd8b3f5af64ae4aed485
Juan Martínez <juan.a.martinez@accenture.com>
2019-11-11 13:10:24 -0500

    Progress in change password

---
+++ EatToday.Prism/EatToday.Prism/Views/ChangePasswordPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace EatToday.Prism.Views
+{
+    public partial class ChangePasswordPage : ContentPage
+    {
+        public ChangePasswordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
